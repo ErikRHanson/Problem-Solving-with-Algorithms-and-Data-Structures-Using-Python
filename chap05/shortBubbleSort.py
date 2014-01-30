fce64455487b889b3fd28d0acd0a0deaa87691f1
Erik Hanson <hoddrofnir@gmail.com>
2014-01-29 22:20:07 -0800

    Add the rest of the examples from Chapter 05

---
+++ chap05/shortBubbleSort.py
@@ -0,0 +1,18 @@
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i]> alist[i+1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        passnum = passnum-1
+
+alist=[20,30,40,90,50,60,70,80,100,110]
+shortBubbleSort(alist)
+
+print(alist)
+        
